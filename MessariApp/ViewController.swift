51b3b1258eec653f94f9cb0114ae7d4543baaa2c
CrazyHearse <3223662@gmail.com>
2022-06-22 13:26:57 +0300

    Initial Commit

---
+++ MessariApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessariApp
+//
+//  Created by Евгений Ерофеев on 22.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
